42864e42d1dbc5db17ffae1b57506716b39c9418
SAEZ Jonathan <saez.jonathan@gmail.com>
2015-06-20 16:40:12 +0200

    Use namespace so we avoid ambiguous class name

--- src/Exceptions/Base.php
+++ src/Exceptions/Base.php
@@ -4,7 +4,7 @@ class Base extends \Exception
 {
     protected $logException = true;
 
-    public function __construct($message = "", $code = 0, Exception $previous = null)
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
         if($this->logException){
